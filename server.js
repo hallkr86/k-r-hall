5089dfd0d1e3bbd13f71f3006f73e91f2a874f11
Kesha Hall <k_renee10@yahoo.com>
2022-02-22 10:56:42 -0500

    added server file

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("client/build"));
+
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
