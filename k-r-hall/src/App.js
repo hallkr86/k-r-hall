2c0e8a6a7048dfc7263d1df77a498284d75f449b
Kesha Hall <k_renee10@yahoo.com>
2022-03-03 13:17:50 -0500

    changed browswerrouter to hash

--- k-r-hall/src/App.js
+++ k-r-hall/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/Home/Home";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
