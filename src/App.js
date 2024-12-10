bd28f0816d15ee9e1d22f3a1400ff41f1fea5e1d
Kesha Hall <k_renee10@yahoo.com>
2024-12-10 18:51:54 -0500

    updating different branches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import {BrowserRouter, Router, Routes, Route} from "react-router-dom";
-=======
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
->>>>>>> master
 import Home from "./components/Home/Home";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
@@ -19,7 +15,7 @@ class App extends Component {
   render(){
 
     return (
-      <BrowserRouter basename="/k-r-hall/">
+      <Router>
     <div className="App">
      
       
@@ -35,7 +31,7 @@ class App extends Component {
       <Footer />
       
     </div>
-    </BrowserRouter>
+    </Router>
     );
   }
   
